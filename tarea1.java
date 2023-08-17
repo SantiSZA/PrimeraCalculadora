497215db8cbe9ad90faa65277422c05c53ed9f31
SantiSZA <142031401+SantiSZA@users.noreply.github.com>
2023-08-17 07:05:32 -0500

    Add files via upload

---
+++ tarea1.java
@@ -0,0 +1,23 @@
+
+package nombre;
+import java.util.Scanner;
+import java.util.Scanner;
+
+
+/**
+ *
+ * Samtiago Zapata Alzate
+ */
+public class tarea1 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+        String name;
+   
+        
+        
+    System.out.println("digite su nombre");
+    name = teclado.nextLine();
+    System.out.println("BIENVENID@ "+name);
+                
+    }
+}
